6cc6de4c9ef74b6e3f382d208bd3aa7e69338bb3
Paul Gölz <pgoelz@cs.cmu.edu>
2022-05-03 15:44:46 -0400

    Update for IJCAI version with polynomial-time approximation algorithm.

--- main_experiments/experiments.py
+++ main_experiments/experiments.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from math import isinf, inf, isnan
+from functools import partial
+from math import ceil, isinf, inf, isnan, log, log1p
 from time import time
 from typing import Iterable, List, Tuple, Optional, Dict, Callable
 
@@ -9,6 +10,7 @@ from gurobipy import Model, quicksum, GRB, setParam
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pyomo.environ as pyo
 import scipy.integrate as integrate
 from scipy.special import betainc, beta
 import scipy.stats as stats
@@ -95,6 +97,54 @@ class Uniform(Dist):
     def max(self):
         return 1.
 
+    def __str__(self):
+        return f"Uniform({self.left_endpoint})"
+
+
+class PiecewiseLinear(Dist):
+    def __init__(self, a: float, base: float = .5):
+        self.a = a
+        self.base = base
+        self.ceiling = 2. - base
+
+    def pdf(self, x) -> float:
+        if x > 1. or x < 0.:
+            return 0.
+        elif x < self.a:
+            return self.base + (self.ceiling - self.base) * x / self.a
+        else:
+            return self.base + (self.ceiling - self.base) * (1. - x) / (1. - self.a)
+
+    def cdf(self, x) -> float:
+        if x <= 0.:
+            return 0.
+        elif x >= 1.:
+            return 1.
+        elif x < self.a:
+            return x * self.base + .5 * (self.ceiling - self.base) / self.a * x**2
+        else:
+            return 1. - (1. - x) * self.base - .5 * (self.ceiling - self.base) / (1. - self.a) * (1. - x)**2
+
+    def sample(self, num: int) -> Iterable[float]:
+        rd = np.random.rand(num, 3)
+        x = np.where(rd[:, 0] < self.base,
+                     rd[:, 1],
+                     np.where(rd[:, 1] < self.a,
+                              self.a * np.sqrt(rd[:, 2]),
+                              1. - (1. - self.a) * np.sqrt(rd[:, 2])))
+        return x
+
+    @property
+    def min(self) -> float:
+        return 0.
+
+    @property
+    def max(self) -> float:
+        return 1.
+
+    def __str__(self):
+        return f"PiecewiseLinear({self.a}, {self.base})"
+
 
 class Beta(Dist):
     def __init__(self, a: float, b: float):
@@ -126,69 +176,67 @@ class Beta(Dist):
     def max(self) -> float:
         return 1.
 
-
-@dataclass
-class BinaryProgression:
-    point_to_try: float
-    recurse_left: Tuple[float, float]
-    recurse_right: Tuple[float, float]
+    def __str__(self):
+        return f"Beta({self.a}, {self.b})"
 
 
-def _binary_progression(left, right, first_multiplier):
-    """Defines which step to take next. Starts with an doubling-argument search to find a finite upper bound,
-    then uses the bisection method."""
-    if isinf(right):
-        if left == 0.:
-            return BinaryProgression(first_multiplier, (left, first_multiplier), (first_multiplier, right))
-        else:
-            # if still no value with probability greater than target found, try double the previous point
-            return BinaryProgression(2 * left, (left, 2 * left), (2 * left, right))
-    else:
-        # bisection method
-        mid = (left + right) / 2
-        return BinaryProgression(mid, (left, mid), (mid, right))
+def _equalization_multipliers(distributions: List[Dist], delta: float, q: float) -> List[float]:
+    n = len(distributions)
+    epsilon = delta / (2 * q)
+    zs = [0 for _ in range(n)]
+
+    def local_eval(x: float, i: int) -> float:
+        z1 = zs[i]
+        prod = distributions[i].pdf(x)
+        for i2, (dist2, z2) in enumerate(zip(distributions, zs)):
+            if i2 != i:
+                prod *= dist2.cdf((1 + epsilon) ** (z1 - z2) * x)
+        return prod
+
+    while True:
+        probs = []
+        for i in range(n):
+            dist = distributions[i]
+            probs.append(integrate.quad(partial(local_eval, i=i), dist.min, dist.max)[0])
+        if all(abs(p - 1/n) <= delta for p in probs):
+            break
+        for i in range(n):
+            if probs[i] <= 1/n:
+                zs[i] += 1
+
+    return [(1 + epsilon)**z for z in zs]
 
 
-def _equalization_multipliers(distributions: List[Dist], start_multiplier: float, fixed_distributions: List[Dist],
-                              fixed_multipliers: List[float], binary_search_iterations: int) -> List[float]:
-    assert binary_search_iterations > 0
+def _equalization_multipliers_refine(distributions: List[Dist], delta: float, q: float) -> List[float]:
+    n = len(distributions)
+    mults = [1. for _ in range(n)]
 
-    if len(distributions) == 0:
-        return []
-    if len(fixed_distributions) == 0:
-        dist = distributions[0]
-        recursive_mults = _equalization_multipliers(distributions[1:], start_multiplier, [dist], [1.],
-                                                    binary_search_iterations)
-        return [1.] + recursive_mults
-    else:
-        dist = distributions[0]
-        n = len(distributions) + len(fixed_distributions)
+    def local_eval(x: float, i: int) -> float:
+        mult1 = mults[i]
+        prod = distributions[i].pdf(x)
+        for i2, (dist2, mult2) in enumerate(zip(distributions, mults)):
+            if i2 != i:
+                prod *= dist2.cdf(mult1 / mult2 * x)
+        return prod
 
-        left = 0.
-        right = inf
+    working_delta = 1.
+    while working_delta > delta:
+        working_delta /= 2.
+        print(working_delta)
+        epsilon = working_delta / (2 * q)
 
-        for _ in range(binary_search_iterations):
-            prog = _binary_progression(left, right, start_multiplier)
-            ptt = prog.point_to_try
+        while True:
+            probs = []
+            for i in range(n):
+                dist = distributions[i]
+                probs.append(integrate.quad(partial(local_eval, i=i), dist.min, dist.max)[0])
+            if all(abs(p - 1/n) <= working_delta for p in probs):
+                break
+            for i in range(n):
+                if probs[i] <= 1/n:
+                    mults[i] *= (1 + epsilon)
 
-            recursive_mults = _equalization_multipliers(distributions[1:], start_multiplier, fixed_distributions+[dist],
-                                                        fixed_multipliers + [ptt], binary_search_iterations)
-
-            def local_eval(x: float) -> float:
-                prod = dist.pdf(x)
-                for dist2, mult in zip(fixed_distributions, fixed_multipliers):
-                    prod *= dist2.cdf(ptt / mult * x)
-                for dist2, mult in zip(distributions[1:], recursive_mults):
-                    prod *= dist2.cdf(ptt / mult * x)
-                return prod
-
-            prob = integrate.quad(local_eval, dist.min, dist.max)[0]
-            if prob < 1/n:
-                left, right = prog.recurse_right
-            else:
-                left, right = prog.recurse_left
-
-        return [ptt] + recursive_mults
+    return mults
 
 
 def round_robin_allocation(utilities: np.ndarray) -> np.ndarray:
@@ -223,6 +271,32 @@ def fractional_mnw_allocation(utilities: np.ndarray) -> np.ndarray:
     return np.argmax(alloc.value, axis=0)
 
 
+def integer_mnw_allocation(utilities: np.ndarray) -> np.ndarray:
+    n, m = utilities.shape
+    model = pyo.ConcreteModel()
+    model.N = pyo.RangeSet(n)
+    model.M = pyo.RangeSet(m)
+    model.alloc = pyo.Var(model.N, model.M, within=pyo.Binary)
+    model.allocate_once = pyo.ConstraintList()
+    for j in model.M:
+        model.allocate_once.add(sum(model.alloc[i, j] for i in model.N) == 1)
+    model.obj = pyo.Objective(expr=sum(pyo.log(sum(utilities[(iint, jint)] * model.alloc[i, j] for j, jint in zip(model.M, range(m))))
+                                       for i, iint in zip(model.N, range(n))),
+                              sense=pyo.maximize)
+    opt = pyo.SolverFactory('baron')
+    opt.solve(model)
+    allocation = []
+    for j in model.M:
+        already_allocated = False
+        for i, iint in zip(model.N, range(n)):
+            if pyo.value(model.alloc[i, j]) > .5:
+                allocation.append(iint)
+                already_allocated = True
+                break
+        assert already_allocated
+    return np.array(allocation)
+
+
 def maximum_envy(utilities: np.ndarray, allocation: np.ndarray) -> float:
     n, m = utilities.shape
     assert allocation.shape == (m,)
@@ -236,6 +310,23 @@ def maximum_envy(utilities: np.ndarray, allocation: np.ndarray) -> float:
 
 def is_envy_free(utilities: np.ndarray, allocation: np.ndarray) -> bool:
     return maximum_envy(utilities, allocation) <= EPS
+
+
+def is_ef1(utilities: np.ndarray, allocation: np.ndarray) -> float:
+    n, m = utilities.shape
+    assert allocation.shape == (m,)
+    alloc = np.zeros((m, n), int)
+    alloc[np.arange(m), allocation] = 1
+    uij = utilities @ alloc
+    uii = np.diag(uij)
+    envy = uij - uii[:, None]
+    most_liked = np.zeros((n, n), float)  # most_liked[i1, i2] = max_{j ￿∈ A_i2} u_i1(j)
+    for i1 in range(n):
+        for i2 in range(n):
+            most_liked[i1, i2] = np.amax(np.multiply(utilities[i1, :], alloc[:, i2]))
+    envy_up_to_1 = envy - most_liked
+
+    return np.amax(envy_up_to_1) <= EPS
 
 
 def is_pareto_optimal(utilities: np.ndarray, allocation: np.ndarray) -> bool:
@@ -262,12 +353,12 @@ def is_pareto_optimal(utilities: np.ndarray, allocation: np.ndarray) -> bool:
     return model.objVal < .001
 
 
-def plot(data, out_path):
+def plot(data, out_path, legend_up: float = 0.):
     fig, ax = plt.subplots(figsize=(7, 3.5))
     plt.xscale("log")
-    linestyles = ["solid", "dotted", "dashed", "dashdot"]
+    linestyles = ["solid", "dotted", "dashed", "dashdot", (0, (3, 5, 1, 5)), (0, (5, 1))]
     palette = color_palette("colorblind")
-    palette = [palette[2], palette[3], palette[0], palette[4]]
+    palette = [palette[2], palette[3], palette[0], palette[4], palette[1]] + palette[5:]
     which_style = 0
     for col in data.columns:
         if col == "m" or col == "iterations":
@@ -277,7 +368,7 @@ def plot(data, out_path):
             left, right = jeffreys_interval(iterations, successes, .95)
             prob = successes / iterations
             errorbars[0, i], errorbars[1, i] = prob - left, right - prob
-        label = col.replace("EF", "envy freeness").replace("PO", "Pareto optimality").replace("mnw", "MNW")
+        label = col.replace("mnw", "MNW").replace("EF ", "envy freeness ").replace("PO", "Pareto optimality")
         ax.errorbar(data["m"], data[col] / data["iterations"], yerr=errorbars, capsize=3.,
                     linestyle=linestyles[which_style % len(linestyles)], color=palette[which_style % len(linestyles)],
                     label=label, ecolor="gray")
@@ -286,36 +377,52 @@ def plot(data, out_path):
     plt.grid(True, axis="y")
     ax.set_xlabel("Number of items")
     ax.set_ylabel("Probability")
-    plt.legend(loc=(.52, .07), prop={'size': 10})
+    plt.legend(loc=(.57, .07 + legend_up), prop={'size': 10})
+    plt.tight_layout()
     fig.savefig(out_path)
 
 
 def experiment_asymmetric(distributions: List[Dist], algorithms: Dict[str, Callable[[np.ndarray], np.ndarray]],
-                          iterations: int, m_values: List[float]) -> pd.DataFrame:
+                          iterations: int, m_values: List[float], run_ef1: bool = False) -> pd.DataFrame:
     data = []
     for m in m_values:
         print("m = ", m)
         np.random.seed(m)
-        ef_counter = {alg: 0 for alg in algorithms if alg != "mnw" or m <= 2000}
-        po_counter = {alg: 0 for alg in algorithms if alg == "round robin" and m <= 2000}
+        ef_counter = {alg: 0 for alg in algorithms if ((alg == "integral mnw" and m < 500)
+                                                       or (alg == "rounded mnw" and m <= 2000) # 2000
+                                                       or "mnw" not in alg)}
+        if run_ef1:
+            ef1_counter = ef_counter.copy()
+        else:
+            ef1_counter = {}
+        po_counter = {alg: 0 for alg in algorithms if alg == "round robin" and m <= 500}
+        m_algorithms = {alg: proc for alg, proc in algorithms.items() if (alg in ef_counter or alg in ef1_counter
+                                                                          or alg in po_counter)}
 
         for num_iter in range(iterations):
+            if num_iter % 100 == 0:
+                print(m, num_iter)
             utilities = np.vstack([dist.sample(m) for dist in distributions])
 
-            allocs = {alg: proc(utilities) for alg, proc in algorithms.items()}
+            allocs = {alg: proc(utilities) for alg, proc in m_algorithms.items()}
 
             for key, alloc in allocs.items():
                 if key in ef_counter and is_envy_free(utilities, alloc):
                     ef_counter[key] += 1
+                if key in ef1_counter and is_ef1(utilities, alloc):
+                    ef1_counter[key] += 1
                 if key in po_counter and is_pareto_optimal(utilities, alloc):
                     po_counter[key] += 1
 
         datum = {"m": m, "iterations": iterations}
         for key, count in ef_counter.items():
             datum["EF " + key] = count
+        for key, count in ef1_counter.items():
+            datum["EF1 " + key] = count
         for key, count in po_counter.items():
             datum["PO " + key] = count
         data.append(datum)
+
     return pd.DataFrame(data)
 
 
@@ -336,45 +443,106 @@ def experiment_symmetric(dist: Dist, n: int, m: int, iterations: int):
     return ef_count
 
 
+def mnw_runtime(distributions: List[Dist], m: int, iterations: int) -> float:
+    np.random.seed(m)
+
+    start = time()
+    for num_iter in range(iterations):
+        print(num_iter)
+        utilities = np.vstack([dist.sample(m) for dist in distributions])
+        integer_mnw_allocation(utilities)
+
+    return (time() - start) / iterations
+
+
 def main():
     setParam("OutputFlag", False)
 
-    wikipedia_distrs = [Beta(.5, .5), Beta(5., 1.), Beta(1., 3.), Beta(2., 2.), Beta(2., 5.)]
     iterations = 1000
 
-    print("\nStart computing multipliers for the five beta distributions.")
+    print("\nStart experiments with triangle-shaped distributions (paper body)")
+    num_linear = 10
+    base = .1
+    distrs = [PiecewiseLinear(i / (num_linear + 1), base) for i in range(1, num_linear + 1)]
+    q = 2. - base
+    special_distr = distrs[4]
+    special_distr_m = 500
+
+    print(f"\nStart computing multipliers {len(distrs)} distributions: {', '.join(str(distr) for distr in distrs)}")
     start = time()
-    multipliers = _equalization_multipliers(wikipedia_distrs, 2., [], [], 20)
+    multipliers = _equalization_multipliers_refine(distrs, .00001, q)
     print(f"Computed multipliers {multipliers} in {time() - start:.0f} seconds.")
-    print("Reference output: [1.0, 0.8339729309082031, 2.0838546752929688, 1.2098731994628906, 2.0375137329101562]")
 
     multipliers = np.array(multipliers)
     algorithms = {"multiplier": lambda utils: multiplier_allocation(utils, multipliers),
-                  "round robin": round_robin_allocation, "mnw": fractional_mnw_allocation}
+                  "round robin": round_robin_allocation, "rounded mnw": fractional_mnw_allocation}
 
     print("\nStart main experiment (Figure 4).")
+    ms = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+    ms = [m for m in ms if m >= len(distrs)]
     start = time()
-    df_main = experiment_asymmetric(wikipedia_distrs, algorithms, iterations,
-                                    [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
+    df_main = experiment_asymmetric(distrs, algorithms, iterations,
+                                    ms)
     print(f"Completed main experiment in {time() - start:.0f} seconds.")
-    df_main.to_csv("fig4.csv", index=False)
-    plot(df_main, "fig4.pdf")
-    print("Wrote output to fig4.csv and fig4.pdf.")
+    df_main.to_csv("fig3.csv", index=False)
+    plot(df_main, "fig3.pdf")
+    print("Wrote output to fig3.csv and fig3.pdf.")
 
     print("\nStart experiment with offset values of m (Figure 5).")
     start = time()
-    df_offset = experiment_asymmetric(wikipedia_distrs, algorithms, iterations,
-                                      [5+3, 10+3, 20+3, 50+3, 100+3, 200+3, 500+3, 1000+3])
+    offset_ms = [m + 3 for m in ms if m < 2000]
+    df_offset = experiment_asymmetric(distrs, algorithms, iterations, offset_ms)
     print(f"Completed offset experiment in {time() - start:.0f} seconds.")
-    df_main.to_csv("fig5.csv", index=False)
-    plot(df_main, "fig5.pdf")
+    df_offset.to_csv("fig5.csv", index=False)
+    plot(df_offset, "fig5.pdf")
     print("Wrote output to fig5.csv and fig5.pdf.")
 
-    print("\nStarting experiment where five agents all have distribution Beta(5, 1).")
-    ef_count = experiment_symmetric(Beta(5., 1.), n=5, m=1000, iterations=iterations)
+    print(f"\nStarting experiment where {len(distrs)} agents all have the same distribution {special_distr}.")
+    start = time()
+    ef_count = experiment_symmetric(special_distr, n=len(distrs), m=special_distr_m, iterations=iterations)
     print(f"Finished symmetric experiment in {time() - start:.0f} seconds.")
     lower_conf, upper_conf = jeffreys_interval(iterations, ef_count, .95)
-    print(f"Result: At m=1000, the welfare-maximizing algorithm is envy free with probability "
+    print(f"Result: At m={special_distr_m}, the welfare-maximizing algorithm is envy free with probability "
+          f"~{ef_count / iterations:.2%} (95% confidence interval: [{lower_conf}, {upper_conf}])")
+
+    print("Measuring MNW runtime")
+    avg_time = mnw_runtime(distrs, 50, 25)
+    print(f"On average, MNW ran in {avg_time} seconds.")
+
+    print("\nStart experiments with Beta distributions (Appendix B.4)")
+    distrs = [Beta(.5, .5), Beta(5., 1.), Beta(1., 3.), Beta(2., 2.), Beta(2., 5.)]
+    special_distr = distrs[1]
+    special_distr_m = 1000
+    q = 5.  # not strictly speaking true, in principle computation could diverge
+
+    print(f"\nStart computing multipliers {len(distrs)} distributions: {', '.join(str(distr) for distr in distrs)}")
+    start = time()
+    multipliers = _equalization_multipliers_refine(distrs, .00001, q)
+    print(f"Computed multipliers {multipliers} in {time() - start:.0f} seconds.")
+
+    multipliers = np.array(multipliers)
+    algorithms = {"multiplier": lambda utils: multiplier_allocation(utils, multipliers),
+                  "round robin": round_robin_allocation, "rounded mnw": fractional_mnw_allocation,
+                  "integral mnw": integer_mnw_allocation}
+
+    print("\nStart main experiment (Figure 6).")
+    ms = [5, 10, 20, 50, 100, 200]
+    ms = [m for m in ms if m >= len(distrs)]
+    start = time()
+    df_main = experiment_asymmetric(distrs, algorithms, iterations, ms, run_ef1=True)
+    print(f"Completed main experiment in {time() - start:.0f} seconds.")
+    df_main.to_csv("fig6.csv", index=False)
+    df_main = df_main[["m", "iterations", "EF multiplier", "EF round robin", "EF rounded mnw", "EF integral mnw",
+                       "EF1 multiplier", "EF1 rounded mnw"]]
+    plot(df_main, "fig6.pdf", legend_up=.4)
+    print("Wrote output to fig6.csv and fig6.pdf.")
+
+    print(f"\nStarting experiment where {len(distrs)} agents all have the same distribution {special_distr}.")
+    start = time()
+    ef_count = experiment_symmetric(special_distr, n=len(distrs), m=special_distr_m, iterations=iterations)
+    print(f"Finished symmetric experiment in {time() - start:.0f} seconds.")
+    lower_conf, upper_conf = jeffreys_interval(iterations, ef_count, .95)
+    print(f"Result: At m={special_distr_m}, the welfare-maximizing algorithm is envy free with probability "
           f"~{ef_count / iterations:.2%} (95% confidence interval: [{lower_conf}, {upper_conf}])")
 
 
